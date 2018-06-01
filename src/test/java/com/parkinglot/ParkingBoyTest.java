a1ec40bb7857f120be47c10d1198e2f172e9a002
David Hau <davidphiliphk@gmail.com>
2018-06-01 14:12:15 +0800

    setup before test case

--- src/test/java/com/parkinglot/ParkingBoyTest.java
+++ src/test/java/com/parkinglot/ParkingBoyTest.java
@@ -1,19 +1,23 @@
 package com.parkinglot;
 
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
 
 public class ParkingBoyTest {
 
-    @Test
-    public void getPreferredParkingLot() {
+    @Before
+    public void setup_parkingSystem() {
         ParkingSystem parkingSystem = ParkingSystem.getParkingSystem();
         parkingSystem.addParkingLot(new ParkingLot(1, 5));
         parkingSystem.addParkingLot(new ParkingLot(2, 10));
         parkingSystem.addParkingLot(new ParkingLot(3, 6));
+    }
 
+    @Test
+    public void getPreferredParkingLot() {
         ParkingBoy parkingBoy = new ParkingBoy(1);
         Assert.assertEquals(2, parkingBoy.getPreferredParkingLot().getId());
     }
